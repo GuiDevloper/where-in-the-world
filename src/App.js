32628ac9464f5e6faf4d7467dfeea50a45e3c51b
GuiDevloper <guicorrea384@gmail.com>
2019-09-11 11:43:15 -0300

    added react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="App-nav">
+        <p className="App-title">
+          Where in the world?
+        </p>
+        <p
+          className="App-night"
+        >
+          Dark Mode
+        </p>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
